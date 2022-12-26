b963961ac72cc2d6700788e40432fa20aabd3c96
MowMowchow <darkstar.hou2@gmail.com>
2022-12-26 12:31:57 -0500

    intial hopepage

---
+++ pages/dump/index.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
+
+export default function Example() {
+  return <div className="relative overflow-hidden bg-white"></div>;
+}
