37248f2d07065833f07ca60b756879fa5ed498a6
MowMowchow <darkstar.hou2@gmail.com>
2022-12-26 14:07:42 -0500

    responsive front

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,17 @@ const navigation = [
 
 export default function Example() {
   return (
-    <div className="grid grid-cols-1">
-      <div className="m-10" />
-      <div className="flex justify-center">
+    <div className="mx-10 grid grid-cols-1">
+      <div className="m-2 md:m-10" />
+      <div className="flex justify-end md:justify-center">
         <Popover className="md:w-7/12 lg:w-2/5">
           <nav>
             <div className="hidden md:flex md:justify-around">
               {navigation.map((item) => (
-                <div className="px-2 py-1 rounded-lg transition ease-in-out delay-75 hover:bg-purple-500 duration-700">
+                <div
+                  key={item.name}
+                  className="px-2 py-1 rounded-lg transition ease-in-out delay-75 hover:bg-purple-500 duration-700"
+                >
                   <Link
                     href={item.href}
                     className="font-poppins font-normal text-2xl hover:text-white duration-500"
@@ -30,11 +33,11 @@ export default function Example() {
                 </div>
               ))}
             </div>
-            <div className="md:hidden flex justify-end">
+            <div className="md:hidden mx-10 my-7 md:m-0">
               <Popover.Button className="outline-none">
                 <div className="px-2 py-1 rounded-lg transition ease-in-out delay-75 hover:bg-teal-500 duration-700 ">
-                  <h1 className="font-poppins font-normal text-3xl hover:text-white duration-500">
-                    more+
+                  <h1 className="font-poppins font-normal text-2xl hover:text-white duration-500">
+                    {"more+"}
                   </h1>
                 </div>
               </Popover.Button>
@@ -49,14 +52,13 @@ export default function Example() {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <Popover.Panel className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden">
+            <Popover.Panel className="md:hidden absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition">
               <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
                 <div className="flex items-center justify-end px-5 pt-4">
                   <Popover.Button className="outline-none">
                     <div className="px-2 py-1 rounded-lg transition ease-in-out delay-75 hover:bg-teal-500 duration-700 ">
                       <h1 className="font-poppins font-normal text-3xl hover:text-white duration-500">
                         <XMarkIcon className="h-6 w-6" />
-                        {/* more+ */}
                       </h1>
                     </div>
                   </Popover.Button>
@@ -77,43 +79,56 @@ export default function Example() {
           </Transition>
         </Popover>
       </div>
-      <div className="m-32" />
-      <div className="flex justify-center">
-        <div className="grid grid-cols-2">
-          {/* <div className="relative w-full h-full flex flex-row justify-center"> */}
-          {/* <div className="relative w-full h-full flex flex-col justify-center"> */}
-          <div className="relative w-96 h-96">
-            <Image
-              src="/he_cycling.png"
-              alt="Picture of the something nice"
-              fill
-              style={{ objectFit: "cover" }}
-            />
-            {/* </div> */}
-            {/* </div> */}
-          </div>
-          <div className="flex flex-col justify-center">
-            <div className="flex flex-col justify-start">
-              <div>
-                <div className="relative">
-                  <div className="absolute bottom-4 left-0">
-                    <h3 className="">Hi I'm,</h3>
+      <div className="m-10 md:m-32" />
+      <div>
+        <div className="flex flex-row justify-center">
+          <div className="flex justify-center md:w-4/5">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+              <div className="hidden md:visible relative w-60 h-60 sm:w-80 sm:h-80 lg:w-96 lg:h-96 md:flex justify-center">
+                <Image
+                  src="/he_cycling.png"
+                  alt="Picture of the something nice"
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <div className="flex flex-col justify-center">
+                <div className="flex flex-col justify-start mb-16 md:m-0">
+                  <div className="relative">
+                    <div className="absolute bottom-1 md:bottom-4 left-0">
+                      <h3 className="text-normal">{"Hi I'm,"}</h3>
+                    </div>
+                  </div>
+                  <div>
+                    <h1 className="text-4xl md:text-7xl lg:text-8xl">
+                      {"Jason Hou"}
+                    </h1>
                   </div>
                 </div>
-                <div>
-                  <h1 className="text-5xl md:text-7xl lg:text-8xl">
-                    Jason Hou
-                  </h1>
+                <div className="flex flex-row justify-center">
+                  <div className="md:hidden relative w-60 h-60 sm:w-80 sm:h-80 lg:w-96 lg:h-96 flex justify-center">
+                    <Image
+                      src="/bike.png"
+                      alt="Picture of the something nice"
+                      fill
+                      style={{ objectFit: "cover" }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="mt-12">
-                <p>
-                  When I'm not writing code I'm biking, when I'm not biking I'm
-                </p>
-                <p>- interested in databases and cloud infra</p>
-                <p>- skilled at AWS, typescript, golang, python... (more )</p>
-
-                <p>- listening to (do actual spotify)</p>
+                <div className="mt-16 md:mt-12">
+                  <p className="text-normal">
+                    {
+                      "When I'm not writing code I'm riding my bike, when I'm not riding my bike I'm"
+                    }
+                  </p>
+                  <ul className="text-normal list-disc list-inside">
+                    <li>{"interested in databases and cloud infra"}</li>
+                    <li>
+                      {"skilled at AWS, typescript, golang, python... (more)"}
+                    </li>
+                    <li>{"listening to (do actual spotify)"}</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
